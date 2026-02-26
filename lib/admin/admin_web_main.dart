b777a8074591026f74bb000de262622edfa100dc
Neel shah <neelshah112002@gmail.com>
2026-02-25 22:47:50 -0500

    Old app crashed , recreating again

--- lib/admin/admin_web_main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:passage/admin/admin_app.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const PassageAdminWebApp());
-}
